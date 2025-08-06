ac20a457bfcba69a73221ec0613866be3439022f
Oliver Isaac <oisaac@gmail.com>
2025-08-05 19:54:27 -0500

    more fixes for servie workers
    Signed-off-by: Oliver Isaac <oisaac@gmail.com>

--- static/serviceWorker.js
+++ static/serviceWorker.js
@@ -6,3 +6,22 @@ self.addEventListener('push', function(event) {
         })
     );
 });
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+
+  event.waitUntil(
+    clients.matchAll({ type: 'window', includeUncontrolled: true }).then(function(clientList) {
+      if (clientList.length > 0) {
+        let client = clientList[0];
+        for (let i = 0; i < clientList.length; i++) {
+          if (clientList[i].focused) {
+            client = clientList[i];
+          }
+        }
+        return client.focus();
+      }
+      return clients.openWindow('https://fanks.isaacinit.com/');
+    })
+  );
+});
